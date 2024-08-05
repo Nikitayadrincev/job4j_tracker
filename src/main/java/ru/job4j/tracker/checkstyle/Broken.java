0eb9e074ada1c8cbf29e7765802e46452040621f
Nikitayadrincev <mr.shove@bk.ru>
2024-08-05 18:58:32 +0300

    6. Правила Checkstyle [#504981]

---
+++ src/main/java/ru/job4j/tracker/checkstyle/Broken.java
@@ -0,0 +1,26 @@
+package ru.job4j.tracker.checkstyle;
+
+public class Broken {
+    private final int sizeOfEmpty = 10;
+
+    String name;
+
+    public String surName;
+
+    public static final String NEW_VALUE = "";
+
+    void echo() {
+    }
+
+    void media(Object obj) {
+        if (obj != null) {
+            System.out.println(obj);
+        }
+    }
+
+    void method(int a, int b, int c, int d, int e, int f, int g) {
+    }
+
+    Broken() {
+    }
+}
