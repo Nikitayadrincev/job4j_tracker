18307b2a68a386d6b466d6777d09f3df7e553910
Nikitayadrincev <mr.shove@bk.ru>
2024-08-15 18:28:37 +0300

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -22,4 +24,30 @@ public class Tracker {
         }
         return rsl;
     }
+
+    public Item[] findByName(String key) {
+        Item[] result = new Item[size];
+        int j = 0;
+        for (int i = 0; i < size; i++) {
+            if (items[i].getName().equals(key)) {
+                result[j] = items[i];
+                j++;
+            }
+        }
+        return Arrays.copyOf(result, j);
+    }
+
+    public Item[] findAll() {
+        Item[] result = new Item[size];
+        int j = 0;
+        for (int i = 0; i < size; i++) {
+            Item name = items[i];
+            if (name != null) {
+                result[j] = name;
+                j++;
+            }
+        }
+        return Arrays.copyOf(result, j);
+    }
 }
+
