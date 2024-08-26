dbbd963f4f041aef4b663c315660eebf8d3cd27d
Nikitayadrincev <mr.shove@bk.ru>
2024-08-26 18:24:12 +0300

    4. Принципы раннего возврата и охранных выражений [#504875 #510406]

--- src/main/java/ru/job4j/early/PasswordValidator.java
+++ src/main/java/ru/job4j/early/PasswordValidator.java
@@ -5,20 +5,21 @@ public class PasswordValidator {
 
     /**
      * Метод проверят пароль по правилам:
-     *  1. Если password null, то выбросить исключение "Password can't be null";
-     *  2. Длина пароля находится в диапазоне [8, 32],
-     *     если нет то "Password should be length [8, 32]";
-     *  3. Пароль содержит хотя бы один символ в верхнем регистре,
-     *     если нет то "Password should contain at least one uppercase letter");
-     *  4. Пароль содержит хотя бы один символ в нижнем регистре,
-     *     если нет то "Password should contain at least one lowercase letter";
-     *  5. Пароль содержит хотя бы одну цифру,
-     *     если нет то"Password should contain at least one figure");
-     *  6. Пароль содержит хотя бы один спец. символ (не цифра и не буква),
-     *     если нет то "Password should contain at least one special symbol");
-     *  7. Пароль не содержит подстрок без учета регистра: qwerty, 12345, password, admin, user.
-     *     Без учета регистра, значит что, например, ни qwerty ни QWERTY ни qwErty и т.п.
-     *     если да, то "Password shouldn't contain substrings: qwerty, 12345, password, admin, user".
+     * 1. Если password null, то выбросить исключение "Password can't be null";
+     * 2. Длина пароля находится в диапазоне [8, 32],
+     * если нет то "Password should be length [8, 32]";
+     * 3. Пароль содержит хотя бы один символ в верхнем регистре,
+     * если нет то "Password should contain at least one uppercase letter");
+     * 4. Пароль содержит хотя бы один символ в нижнем регистре,
+     * если нет то "Password should contain at least one lowercase letter";
+     * 5. Пароль содержит хотя бы одну цифру,
+     * если нет то"Password should contain at least one figure");
+     * 6. Пароль содержит хотя бы один спец. символ (не цифра и не буква),
+     * если нет то "Password should contain at least one special symbol");
+     * 7. Пароль не содержит подстрок без учета регистра: qwerty, 12345, password, admin, user.
+     * Без учета регистра, значит что, например, ни qwerty ни QWERTY ни qwErty и т.п.
+     * если да, то "Password shouldn't contain substrings: qwerty, 12345, password, admin, user".
+     *
      * @param password Пароль
      * @return Вернет пароль или выбросит исключение.
      */
@@ -50,6 +51,9 @@ public class PasswordValidator {
             if (!Character.isLetterOrDigit(symbol)) {
                 hasSpecial = true;
             }
+            if (hasUpCase && hasLowCase && hasDigit && hasSpecial) {
+                break;
+            }
         }
         if (!hasUpCase) {
             throw new IllegalArgumentException(
@@ -71,10 +75,11 @@ public class PasswordValidator {
                     "Password should contain at least one special symbol"
             );
         }
-        for (int i = 0; i < FORBIDDEN.length; i++) {
-            if (password.toLowerCase().contains(FORBIDDEN[i].toLowerCase())) {
+        String passwordLowerCase = password.toLowerCase();
+        for (String num : FORBIDDEN) {
+            if (passwordLowerCase.contains(num)) {
                 throw new IllegalArgumentException(
-                       "Password shouldn't contain substrings: qwerty, 12345, password, admin, user"
+                        "Password shouldn't contain substrings: qwerty, 12345, password, admin, user"
                 );
             }
         }
